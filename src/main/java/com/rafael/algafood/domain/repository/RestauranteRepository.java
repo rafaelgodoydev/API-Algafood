131f7c7c351ad396360507ff94929aaf6fed7c90
rafaelgodoydev <rafaeldevcloud01@gmail.com>
2024-02-02 17:22:47 -0300

    Implementando queries JPQL customizadas com @Query

--- src/main/java/com/rafael/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/rafael/algafood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.rafael.algafood.domain.repository;
 
 import com.rafael.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -12,6 +14,9 @@ import java.util.Optional;
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 
