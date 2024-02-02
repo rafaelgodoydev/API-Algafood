eabcbbb1053b8545bb2024592b3f493892b12da4
rafaelgodoydev <rafaeldevcloud01@gmail.com>
2024-02-02 15:45:55 -0300

    Usando as keywords para definir critérios de query methods

--- src/main/java/com/rafael/algafood/api/controller/TesteController.java
+++ src/main/java/com/rafael/algafood/api/controller/TesteController.java
@@ -1,11 +1,15 @@
 package com.rafael.algafood.api.controller;
 
 import com.rafael.algafood.domain.model.Cozinha;
+import com.rafael.algafood.domain.model.Restaurante;
 import com.rafael.algafood.domain.repository.CozinhaRepository;
+import com.rafael.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -14,8 +18,46 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-//    @GetMapping("/cozinhas/por-nome")
-//    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//        return cozinhaRepository.consultarPorNome(nome);
-//    }
+    @Autowired
+    RestauranteRepository restauranteRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/exists")
+    public boolean cozinhaExists(String nome) {
+        return cozinhaRepository.existsByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.queryByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantePorNome(String nome, Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+    }
+
+    @GetMapping("/restaurantes/primeiro-por-nome")
+    public Optional<Restaurante> restaurantesPrimeiroPorNome(String nome) {
+        return restauranteRepository.findFirstRestauranteByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/top2-por-nome")
+    public List<Restaurante> restaurantesTop2PorNome(String nome) {
+        return restauranteRepository.findTop2ByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/count-por-cozinha")
+    public int restaurantesCountPorCozinha(Long cozinhaId) {
+        return restauranteRepository.countByCozinhaId(cozinhaId);
+    }
 }
--- src/main/java/com/rafael/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/rafael/algafood/domain/repository/RestauranteRepository.java
@@ -4,6 +4,20 @@ import com.rafael.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+
+    Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinhaId);
 }
