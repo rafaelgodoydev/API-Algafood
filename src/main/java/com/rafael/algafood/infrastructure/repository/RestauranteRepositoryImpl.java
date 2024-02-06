3389452a00758a08d45516a58d5d003c45839fc5
rafaelgodoydev <rafaeldevcloud01@gmail.com>
2024-02-06 14:44:50 -0300

    Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/rafael/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/rafael/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,26 +1,32 @@
 package com.rafael.algafood.infrastructure.repository;
 
 import com.rafael.algafood.domain.model.Restaurante;
+import com.rafael.algafood.domain.repository.RestauranteRepository;
 import com.rafael.algafood.domain.repository.RestauranteRepositoryQueries;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.rafael.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.rafael.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @PersistenceContext
     private EntityManager manager;
+
+    @Autowired
+    @Lazy
+    private RestauranteRepository restauranteRepository;
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
@@ -47,5 +53,10 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
         var query = manager.createQuery(criteria);
         return query.getResultList();
+    }
+
+    @Override
+    public List<Restaurante> findComFreteGratis(String nome) {
+        return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
     }
 }
